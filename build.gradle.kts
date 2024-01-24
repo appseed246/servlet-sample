97f9462e17139699b80c3b4a03b67e82b7c57195
appseed246 <guitar.telecaster2017@gmail.com>
2024-01-25 01:35:50 +0900

    groovy追加

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,9 +16,11 @@ dependencies {
 
     compileOnly("org.projectlombok:lombok:1.18.30")
     annotationProcessor("org.projectlombok:lombok:1.18.30")
-    
+
     testCompileOnly("org.projectlombok:lombok:1.18.30")
     testAnnotationProcessor("org.projectlombok:lombok:1.18.30")
+
+    implementation("org.codehaus.groovy:groovy-all:3.0.20")
 }
 
 tasks.test {
